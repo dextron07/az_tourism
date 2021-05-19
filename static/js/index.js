7ce08f58d9009adfe9d72002919b78cea514b2cf
Vipul Gupta <vgupta@Vipuls-MacBook-Air.local>
2021-05-20 01:08:41 +0530

    Initial Commit

---
+++ static/js/index.js
@@ -0,0 +1,61 @@
+const sideBar = document.querySelector('#side-bar');
+
+const changeSideBar = () => {
+    let showcaseAppear = document.querySelector('.images');
+    let showcaseAppearPosition = showcaseAppear.getBoundingClientRect().top * 2;
+
+    let popularDestinationAppear = document.querySelector('#section-destinations .slide-show');
+    let popularDestinationAppearPosition = popularDestinationAppear.getBoundingClientRect().top;
+
+    let popularPackageAppear = document.querySelector('#section-packages .slide-show');
+    let popularPackageAppearPosition = popularPackageAppear.getBoundingClientRect().top;
+
+    let servicesAppear = document.querySelector('.section-services-cards');
+    let servicesAppearPosition = servicesAppear.getBoundingClientRect().top;
+
+    let screenPosition = window.innerHeight / 1.3;
+
+    let bar1 = document.querySelector('#bar-1');
+    let bar2 = document.querySelector('#bar-2');
+    let bar3 = document.querySelector('#bar-3');
+    let bar4 = document.querySelector('#bar-4');
+
+    if (showcaseAppearPosition < screenPosition) {
+        bar1.classList.add('bar-active');
+        bar2.classList.remove('bar-active');
+        bar3.classList.remove('bar-active');
+        bar4.classList.remove('bar-active');
+
+        popularPackageAppear.classList.remove('slide-show-appear');
+        popularDestinationAppear.classList.remove('slide-show-appear');
+    }
+
+    if (popularDestinationAppearPosition < screenPosition) {
+        bar1.classList.remove('bar-active');
+        bar3.classList.remove('bar-active');
+        bar4.classList.remove('bar-active');
+        bar2.classList.add('bar-active');
+
+        popularDestinationAppear.classList.add('slide-show-appear');
+        popularPackageAppear.classList.remove('slide-show-appear');
+    }
+
+    if (popularPackageAppearPosition < screenPosition) {
+        bar2.classList.remove('bar-active');
+        bar1.classList.remove('bar-active');
+        bar3.classList.add('bar-active');
+        bar4.classList.remove('bar-active');
+
+        popularPackageAppear.classList.add('slide-show-appear');
+        popularDestinationAppear.classList.remove('slide-show-appear');
+    }
+
+    if (servicesAppearPosition < screenPosition) {
+        bar1.classList.remove('bar-active');
+        bar2.classList.remove('bar-active');
+        bar3.classList.remove('bar-active');
+        bar4.classList.add('bar-active');
+    }
+}
+
+window.addEventListener('scroll', changeSideBar);
